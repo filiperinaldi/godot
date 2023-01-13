b7fa1aa7bdc9e038c97bcc360f6a4d6a59dd3f50
Filipe Rinaldi <filipe.rinaldi@gmail.com>
2023-01-13 05:41:13 +0000

    Wayland: Add initial window support
    * Add main window support
    * Switch from the Dummy rasterizer to OpenGL3
    * Integrate EGLManager

--- platform/linuxbsd/wayland/display_server_wayland.cpp
+++ platform/linuxbsd/wayland/display_server_wayland.cpp
@@ -32,7 +32,14 @@
 #include "core/os/memory.h"
 #include "display_server_wayland.h"
 
-#include "servers/rendering/dummy/rasterizer_dummy.h"
+#if defined(GLES3_ENABLED)
+#include "drivers/gles3/rasterizer_gles3.h"
+#define _HAS_RASTERIZER
+#endif
+
+#if !defined(_HAS_RASTERIZER)
+	#error "Wayland: No compatible rasterizer has been defined in the build"
+#endif
 
 #if 1
 	#define DEBUG_LOG_WAYLAND(...) \
@@ -47,7 +54,9 @@
 
 Vector<String> DisplayServerWayland::get_rendering_drivers() {
 	Vector<String> drivers;
-	drivers.push_back("dummy");
+#if defined(GLES3_ENABLED)
+	drivers.push_back("opengl3");
+#endif
 	return drivers;
 }
 
@@ -69,6 +78,16 @@ void DisplayServerWayland::h_wl_global_registry_global(void *data, struct wl_reg
 	if (strcmp(interface, wl_compositor_interface.name) == 0) {
 		display->compositor = (struct wl_compositor *)wl_registry_bind(registry,
 			name, &wl_compositor_interface, MIN(version, wl_compositor_interface.version));
+
+	} else if (strcmp(interface, wl_shm_interface.name) == 0) {
+		display->shm = (struct wl_shm *)wl_registry_bind(registry,
+			name, &wl_shm_interface, MIN(version, wl_shm_interface.version));
+
+	} else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
+		display->xdg_wm_base = (struct xdg_wm_base *)wl_registry_bind(registry,
+			name, &xdg_wm_base_interface, MIN(version, xdg_wm_base_interface.version));
+		xdg_wm_base_add_listener(display->xdg_wm_base, &xdg_wm_base_listener, display);
+
 	}
 #if defined(DEV_ENABLED)
 	else {
@@ -79,6 +98,102 @@ void DisplayServerWayland::h_wl_global_registry_global(void *data, struct wl_reg
 
 void DisplayServerWayland::h_wl_global_registry_remove(void *data, struct wl_registry *registry, uint32_t name) {
 	DEBUG_LOG_WAYLAND("Ignoring removal of object 0x%08x\n", name);
+}
+
+void DisplayServerWayland::h_xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base, uint32_t serial) {
+	WDisplay *display = (WDisplay *)data;
+	xdg_wm_base_pong(display->xdg_wm_base, serial);
+	DEBUG_LOG_WAYLAND("...pong (%d)\n", serial);
+}
+
+void DisplayServerWayland::h_xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, uint32_t serial) {
+	WWindow *w = (WWindow *)data;
+	xdg_surface_ack_configure(w->xdg_surface, serial);
+	w->pending_config = false;
+}
+
+void DisplayServerWayland::h_wl_buffer_release(void *data, struct wl_buffer *wl_buffer) {
+	wl_buffer_destroy(wl_buffer);
+}
+
+DisplayServer::WindowID DisplayServerWayland::_window_create(WindowMode p_mode, VSyncMode p_vsync_mode, uint32_t p_flags, const Vector2i *p_position, const Vector2i &p_resolution) {
+	WWindow *w = memnew(WWindow);
+	ERR_FAIL_NULL_V_MSG(w, INVALID_WINDOW_ID, "Wayland: Failed to allocate memory for window");
+
+	w->mode = p_mode;
+	w->flags = p_flags;
+	w->vsync_mode = p_vsync_mode;
+	w->size = p_resolution;
+
+	w->wl_surface = wl_compositor_create_surface(display.compositor);
+	if (!w->wl_surface) {
+		_window_destroy(w);
+		ERR_FAIL_V_MSG(INVALID_WINDOW_ID, "Wayland: Failed to create surface");
+	}
+
+	w->xdg_surface = xdg_wm_base_get_xdg_surface(display.xdg_wm_base, w->wl_surface);
+	if (!w->xdg_surface) {
+		_window_destroy(w);
+		ERR_FAIL_V_MSG(INVALID_WINDOW_ID, "Wayland: Failed to create xdg surface");
+	}
+	xdg_surface_add_listener(w->xdg_surface, &xdg_surface_listener, w);
+
+	w->xdg_toplevel = xdg_surface_get_toplevel(w->xdg_surface);
+	if (!w->xdg_toplevel) {
+		_window_destroy(w);
+		ERR_FAIL_V_MSG(INVALID_WINDOW_ID, "Wayland: Failed to create top-level xdg surface");
+	}
+
+	xdg_toplevel_set_title(w->xdg_toplevel, "Godot");
+	w->pending_config = true;
+	wl_surface_commit(w->wl_surface);
+
+	while (w->pending_config) {
+		wl_display_dispatch(display.display);
+	}
+
+#if defined(GLES3_ENABLED)
+	if (display.egl_manager) {
+		w->native = wl_egl_window_create(w->wl_surface, p_resolution.width, p_resolution.height);
+		if (w->native == EGL_NO_SURFACE) {
+			_window_destroy(w);
+			ERR_FAIL_V_MSG(INVALID_WINDOW_ID, "Wayland: Failed to create native EGL surface");
+		}
+
+		Error ret = display.egl_manager->window_create(w->egl_window, w->native);
+		if (ret != OK) {
+			_window_destroy(w);
+			ERR_FAIL_V_MSG(INVALID_WINDOW_ID, "Wayland: Failed to create EGL window");
+		}
+
+		RasterizerGLES3::make_current();
+	}
+#endif
+
+	WindowID id = windows.find(nullptr);
+	if (id < 0)
+		id = windows.size();
+	windows.insert(id, w);
+	current_window = w;
+
+	return id;
+}
+
+void DisplayServerWayland::_window_destroy(WWindow *window) {
+	if (window == nullptr)
+		return;
+	if (window->xdg_toplevel)
+		xdg_toplevel_destroy(window->xdg_toplevel);
+	if (window->xdg_surface)
+		xdg_surface_destroy(window->xdg_surface);
+	if (window->wl_surface)
+		wl_surface_destroy(window->wl_surface);
+	if (window->native)
+		wl_egl_window_destroy(window->native);
+#if defined(GLES3_ENABLED)
+	if (display.egl_manager)
+		display.egl_manager->window_destroy(window->egl_window);
+#endif
 }
 
 DisplayServerWayland::DisplayServerWayland(const String &p_rendering_driver, WindowMode p_mode, VSyncMode p_vsync_mode, uint32_t p_flags, const Vector2i *p_position, const Vector2i &p_resolution, int p_screen, Error &r_error) {
@@ -95,18 +210,107 @@ DisplayServerWayland::DisplayServerWayland(const String &p_rendering_driver, Win
 	wl_display_roundtrip(display.display);
 
 	ERR_FAIL_NULL_MSG(display.compositor, "Wayland: Failed to acquire compositor");
+	ERR_FAIL_NULL_MSG(display.xdg_wm_base, "Wayland: Failed to acquire xdg_wm_base");
 
-	RasterizerDummy::make_current();
+#if defined(GLES3_ENABLED)
+	if (p_rendering_driver == "opengl3") {
+		display.egl_manager = memnew(EGLManager(EGL_PLATFORM_WAYLAND_KHR, display.display));
+		ERR_FAIL_NULL_MSG(display.egl_manager, "Wayland: Failed to create EGL manager");
+
+		Error ret = display.egl_manager->initialise();
+		ERR_FAIL_COND_MSG(ret != OK, "Wayland: Failed initialise EGL manager");
+	}
+#endif
+
+	wl_display_roundtrip(display.display);
+
+	DEBUG_LOG_WAYLAND("Creating main window...\n");
+	WindowID window_id = _window_create(p_mode, p_vsync_mode, p_flags, p_position, p_resolution);
+	ERR_FAIL_COND_MSG(window_id == INVALID_WINDOW_ID, "Wayland: Failed to create main window");
 
 	r_error = OK;
+}
+
+Vector<DisplayServer::WindowID> DisplayServerWayland::get_window_list() const {
+	_THREAD_SAFE_METHOD_
+
+	Vector<DisplayServer::WindowID> window_ids;
+
+	for (int i = 0; i < windows.size(); i++) {
+		if (windows[i] != nullptr) {
+			window_ids.push_back(i);
+		}
+	}
+	return window_ids;
+}
+
+void DisplayServerWayland::swap_buffers() {
+#if defined(GLES3_ENABLED)
+	if (display.egl_manager)
+		display.egl_manager->swap_buffers();
+#endif
+}
+
+bool DisplayServerWayland::has_feature(Feature p_feature) const {
+	switch (p_feature) {
+	// Supported
+	case FEATURE_SWAP_BUFFERS:
+		return true;
+
+	// Not implemented yet (show debug messages for now)
+	case FEATURE_SUBWINDOWS:
+	case FEATURE_TOUCHSCREEN:
+	case FEATURE_MOUSE:
+	case FEATURE_MOUSE_WARP:
+	case FEATURE_CLIPBOARD:
+	case FEATURE_CURSOR_SHAPE:
+	case FEATURE_CUSTOM_CURSOR_SHAPE:
+	case FEATURE_IME:
+	case FEATURE_WINDOW_TRANSPARENCY:
+	case FEATURE_HIDPI:
+	case FEATURE_ORIENTATION:
+	case FEATURE_KEEP_SCREEN_ON:
+	case FEATURE_CLIPBOARD_PRIMARY:
+	case FEATURE_TEXT_TO_SPEECH:
+		DEBUG_LOG_WAYLAND("Feature %d not implemented\n", p_feature);
+		break;
+
+	default:
+		return false;
+	}
+
+	return false;
+}
+
+void DisplayServerWayland::window_set_title(const String &p_title, WindowID p_window) {
+	_THREAD_SAFE_METHOD_
+
+	WWindow *w = _get_window_from_id(p_window);
+	if (w)
+		xdg_toplevel_set_title(w->xdg_toplevel, p_title.utf8().get_data());
+}
+
+Size2i DisplayServerWayland::window_get_size(WindowID p_window) const {
+	_THREAD_SAFE_METHOD_
+
+	WWindow *w;
+
+	if ((p_window >= 0) && (p_window < windows.size()) && ((w = windows[p_window]) != nullptr)) {
+		return w->size;
+	}
+
+	return Size2i();
 }
 
 DisplayServerWayland::~DisplayServerWayland() {
 	if (display.compositor)
 		wl_compositor_destroy(display.compositor);
-
 	if (display.registry)
 		wl_registry_destroy(display.registry);
+
+	for (unsigned int i = 0; i < windows.size(); i++) {
+		_window_destroy(windows[i]);
+	}
 
 	if (display.display) {
 		wl_display_flush(display.display);
--- platform/linuxbsd/wayland/display_server_wayland.h
+++ platform/linuxbsd/wayland/display_server_wayland.h
@@ -33,7 +33,14 @@
 
 #include <wayland-client.h>
 
+#include "core/templates/local_vector.h"
 #include "servers/display_server.h"
+#include "xdg-shell.gen.h"
+
+#if defined(GLES3_ENABLED)
+#include <wayland-egl.h>
+#include "egl_manager.h"
+#endif
 
 
 class DisplayServerWayland : public DisplayServer {
@@ -43,11 +50,26 @@ private:
 	// Wayland handlers
 	static void h_wl_global_registry_global(void *data, struct wl_registry *registry, uint32_t name, const char *interface, uint32_t version);
 	static void h_wl_global_registry_remove(void *data, struct wl_registry *registry, uint32_t name);
+	static void h_xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base, uint32_t serial);
+	static void h_xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, uint32_t serial);
+	static void h_wl_buffer_release(void *data, struct wl_buffer *wl_buffer);
 
 	// Wayland listeners
 	static constexpr struct wl_registry_listener registry_listener = {
 		.global = h_wl_global_registry_global,
 		.global_remove = h_wl_global_registry_remove,
+	};
+
+	static constexpr struct xdg_wm_base_listener xdg_wm_base_listener = {
+		.ping = h_xdg_wm_base_ping,
+	};
+
+	static constexpr struct xdg_surface_listener xdg_surface_listener = {
+		.configure = h_xdg_surface_configure,
+	};
+
+	static constexpr struct wl_buffer_listener wl_buffer_listener = {
+		.release = h_wl_buffer_release,
 	};
 
 	// Wayland related context
@@ -56,7 +78,37 @@ private:
 		struct wl_display *display = nullptr;
 		struct wl_compositor *compositor = nullptr;
 		struct wl_registry *registry = nullptr;
+		struct wl_shm *shm = nullptr;
+		struct xdg_wm_base *xdg_wm_base = nullptr;
+#if defined(GLES3_ENABLED)
+		EGLManager *egl_manager = nullptr;
+#endif
 	} display;
+
+	struct WWindow {
+		WindowMode mode;
+		VSyncMode vsync_mode;
+		uint32_t flags;
+		Vector2i position;
+		Vector2i resolution;
+		Size2i size;
+
+		// Wayland related
+		struct wl_surface *wl_surface = nullptr;
+		struct xdg_surface *xdg_surface = nullptr;
+		struct xdg_toplevel *xdg_toplevel = nullptr;
+		bool pending_config = true;
+#if defined(GLES3_ENABLED)
+		struct wl_egl_window *native = nullptr;
+		EGLManager::Window egl_window;
+#endif
+	};
+
+	WWindow *current_window = nullptr;
+	LocalVector<WWindow *> windows;
+
+	DisplayServer::WindowID _window_create(WindowMode p_mode, VSyncMode p_vsync_mode, uint32_t p_flags, const Vector2i *p_position, const Vector2i &p_resolution);
+	void _window_destroy(WWindow *window);
 
 public:
 	static DisplayServer *create(const String &p_rendering_driver, WindowMode p_mode, VSyncMode p_vsync_mode, uint32_t p_flags, const Vector2i *p_position, const Vector2i &p_resolution, int p_screen, Error &r_error);
@@ -67,9 +119,13 @@ public:
 	~DisplayServerWayland();
 
 	String get_name() const override { return "Wayland"; }
+	Vector<DisplayServer::WindowID> get_window_list() const override;
+	virtual void swap_buffers() override;
+	bool has_feature(Feature p_feature) const override;
+	void window_set_title(const String &p_title, WindowID p_window = MAIN_WINDOW_ID) override;
+	Size2i window_get_size(WindowID p_window = MAIN_WINDOW_ID) const override;
 
 	/* Not implemented yet */
-	bool has_feature(Feature p_feature) const override { WARN_PRINT_ONCE("Not implemented"); return false; }
 	int get_screen_count() const override { WARN_PRINT_ONCE("Not implemented"); return 0; }
 	int get_primary_screen() const override { WARN_PRINT_ONCE("Not implemented"); return 0; };
 	Point2i screen_get_position(int p_screen = SCREEN_OF_MAIN_WINDOW) const override { WARN_PRINT_ONCE("Not implemented"); return Point2i(); }
@@ -77,7 +133,6 @@ public:
 	Rect2i screen_get_usable_rect(int p_screen = SCREEN_OF_MAIN_WINDOW) const override { WARN_PRINT_ONCE("Not implemented"); return Rect2i(); }
 	int screen_get_dpi(int p_screen = SCREEN_OF_MAIN_WINDOW) const override { WARN_PRINT_ONCE("Not implemented"); return 0; }
 	float screen_get_refresh_rate(int p_screen = SCREEN_OF_MAIN_WINDOW) const override { WARN_PRINT_ONCE("Not implemented"); return 0.0; }
-	Vector<DisplayServer::WindowID> get_window_list() const override { WARN_PRINT_ONCE("Not implemented"); return Vector<DisplayServer::WindowID>(); }
 	WindowID get_window_at_screen_position(const Point2i &p_position) const override { WARN_PRINT_ONCE("Not implemented"); return 0; }
 	void window_attach_instance_id(ObjectID p_instance, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
 	ObjectID window_get_attached_instance_id(WindowID p_window = MAIN_WINDOW_ID) const override { WARN_PRINT_ONCE("Not implemented"); return ObjectID(); }
@@ -86,7 +141,6 @@ public:
 	void window_set_input_event_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
 	void window_set_input_text_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
 	void window_set_drop_files_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
-	void window_set_title(const String &p_title, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
 	int window_get_current_screen(WindowID p_window = MAIN_WINDOW_ID) const override { WARN_PRINT_ONCE("Not implemented"); return 0; }
 	void window_set_current_screen(int p_screen, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
 	Point2i window_get_position(WindowID p_window = MAIN_WINDOW_ID) const override { WARN_PRINT_ONCE("Not implemented"); return Point2i(); }
@@ -98,7 +152,6 @@ public:
 	void window_set_min_size(const Size2i p_size, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
 	Size2i window_get_min_size(WindowID p_window = MAIN_WINDOW_ID) const override { WARN_PRINT_ONCE("Not implemented"); return Size2i(); }
 	void window_set_size(const Size2i p_size, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
-	Size2i window_get_size(WindowID p_window = MAIN_WINDOW_ID) const override { WARN_PRINT_ONCE("Not implemented"); return Size2i(); }
 	Size2i window_get_size_with_decorations(WindowID p_window = MAIN_WINDOW_ID) const override { WARN_PRINT_ONCE("Not implemented"); return Size2i(); }
 	void window_set_mode(WindowMode p_mode, WindowID p_window = MAIN_WINDOW_ID) override { WARN_PRINT_ONCE("Not implemented"); return; }
 	WindowMode window_get_mode(WindowID p_window = MAIN_WINDOW_ID) const override { WARN_PRINT_ONCE("Not implemented"); return WINDOW_MODE_WINDOWED; }
